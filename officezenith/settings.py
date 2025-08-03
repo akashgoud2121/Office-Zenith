df5794a96de3569d93b2a4eb360aec9519b5924e
Akash <letsmail.akashgoud@gmail.com>
2025-08-03 17:14:28 +0530

    Updated vercel

--- officezenith/settings.py
+++ officezenith/settings.py
@@ -119,11 +119,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'reminders/static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-# Add this for static files handling
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
